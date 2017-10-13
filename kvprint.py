b18b09f0af1c4a3d58ee59110f8d76d3c3efe4fe
Alex Forbes <al4@users.noreply.github.com>
2017-10-13 14:14:02 +0100

    Support setting width, break out kvalign function

--- kvprint.py
+++ kvprint.py
@@ -14,27 +14,36 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def kvprint(data, separator=': '):
-    """ Print a dictionary of key values aligned by separator
+def kvalign(data, max_width=None):
+    """ Return a list of lines, with the keys left-padded to the length of
+    the longest key or max_width, whichever is shorter.
     """
     longest_key = max([len(k) for k in data.keys()])
     logger.debug('longest_key: {}'.format(longest_key))
 
+    if max_width:
+        column_width = longest_key if longest_key < max_width else max_width
+    else:
+        column_width = longest_key
+    logger.debug('column_width: {}'.format(column_width))
+
     lines = []
     for key, value in data.items():
-        print('{0:>{x}}'.format('foo', x=1))
-        print('key: {}'.format(key))
-        print('value: {}'.format(value))
-
         lines.append([
-            '{0:>{x}}'.format(key, x=longest_key),
+            '{0:>{x}}'.format(str(key), x=column_width)[:column_width],
             value
         ])
 
-    for l in lines:
+    return lines
+
+
+def kvprint(data, separator=': ', width=None):
+    """ Print a dictionary of key values aligned by separator
+    """
+    for l in kvalign(data, max_width=width):
         print(separator.join([str(v) for v in l]))
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    kvprint({'foo': 'bar', 'ilohsdgudlfhg': 123})
+    kvprint({'foo': 'bar', 'longer key string': 123}, width=4)
